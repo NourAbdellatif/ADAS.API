691b7186790ce107b654c564053117725e27761f
Nour Abdellatif <noor.abdelatif@gmail.com>
2023-06-25 21:06:51 +0300

    Working migration

---
+++ ADAS.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using ADAS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ADAS.Infrastructure.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+	public void Configure(EntityTypeBuilder<User> builder)
+	{
+		builder.Property(u => u.Id)
+			.ValueGeneratedOnAdd();
+
+		builder.Property(u => u.Email)
+			.IsRequired();
+	}
+}
