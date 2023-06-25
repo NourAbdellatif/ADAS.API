16afdfad65d0c0b7c07f66bd748bbf041a358823
Nour Abdellatif <noor.abdelatif@gmail.com>
2023-06-25 22:02:46 +0300

    Add validation

--- ADAS.API/Program.cs
+++ ADAS.API/Program.cs
@@ -1,13 +1,23 @@
+using ADAS.API.Extensions;
 using ADAS.Application.DIExtensions;
 using ADAS.Infrastructure.DIExtensions;
 using ADAS.Infrastructure.Persistence;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+	.AddFluentValidation(options =>
+	{
+		options.RegisterValidatorsFromAssembly(typeof(ApplicationDIExtensions).Assembly);
+	})
+	.ConfigureApiBehaviorOptions(options =>
+	{
+		options.InvalidModelStateResponseFactory = context => context.HandleInvalidRequest();
+	});;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -28,7 +38,6 @@ var scope = app.Services.CreateAsyncScope();
 var db = scope.ServiceProvider.GetService<AdasDBContext>();
 await db.Database.MigrateAsync();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
