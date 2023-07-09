c21ab3d321f8c4d5edc801c146e5313c8997f004
Nour Abdellatif <noor.abdelatif@gmail.com>
2023-07-09 20:36:06 +0300

    Validate bug report

--- ADAS.Application/BusinessLogic/Bugs/Commands/CreateBugTicket/CreateBugTicketCommandValidator.cs
+++ ADAS.Application/BusinessLogic/Bugs/Commands/CreateBugTicket/CreateBugTicketCommandValidator.cs
@@ -19,5 +19,12 @@ public class CreateBugTicketCommandValidator : AbstractValidator<CreateBugTicket
 			.NotEmpty()
 			.Must(title => title.Length < FieldLength.Small)
 			.WithMessage("Title cannot be empty");
+		
+		RuleFor(c => c)
+			.Must(c =>
+			{
+				return !_context.BugTickets.Any(bt => bt.Title == c.Title && bt.CarId == c.CarId);
+			})
+			.WithMessage("Bug with this title already exists for this car");
 	}
 }
